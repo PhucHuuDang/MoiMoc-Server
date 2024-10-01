32baf8fd0d8da818282da8233f7e1b897e8eea52
HarryDang <danghuuphuc001@gmail.com>
2024-10-01 15:44:38 +0700

    fix: change to relative path

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,7 +16,7 @@ import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { NewUser } from "../drizzle/schema/user.schema";
-import { JwtAuthGuard } from "src/auth/guards/jwt-auth/jwt-auth.guard";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth/jwt-auth.guard";
 
 @Controller("users")
 export class UserController {
