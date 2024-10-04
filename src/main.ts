a3ff911aaec6bd62149a999e8fa8dac4dfaaeca4
HarryDang <danghuuphuc001@gmail.com>
2024-10-04 13:06:38 +0700

    fix(CORS): enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,18 @@ import { ExpressAdapter } from "@nestjs/platform-express";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // app.enableCors({
+  //   origin: ["http://localhost:3000/", "https://moi-moc-client.vercel.app/"],
+  //   methods: ["POST", "PUT", "DELETE", "GET"],
+  // });
+
+  app.enableCors({
+    origin: [process.env.LOCAL_DOMAIN, process.env.PRODUCTION_DOMAIN],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  });
+
   await app.listen(3002);
 }
 bootstrap();
