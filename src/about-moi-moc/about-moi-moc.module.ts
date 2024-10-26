02de1ce0a85fa958503ee6df0ac8efbb29734bea
HarryDang <danghuuphuc001@gmail.com>
2024-10-26 23:45:39 +0700

    reafactor(about-moi-moc): add drizzle module

--- src/about-moi-moc/about-moi-moc.module.ts
+++ src/about-moi-moc/about-moi-moc.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AboutMoiMocService } from './about-moi-moc.service';
-import { AboutMoiMocController } from './about-moi-moc.controller';
+import { Module } from "@nestjs/common";
+import { AboutMoiMocService } from "./about-moi-moc.service";
+import { AboutMoiMocController } from "./about-moi-moc.controller";
+import { DrizzleModule } from "src/drizzle/drizzle.module";
 
 @Module({
   controllers: [AboutMoiMocController],
   providers: [AboutMoiMocService],
+  imports: [DrizzleModule],
 })
 export class AboutMoiMocModule {}
